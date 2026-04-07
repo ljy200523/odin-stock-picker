fd396b7d4fba9a1969a4d436a9d99a236ea833e6
Ian Lam <ljy200523@gmail.com>
2026-04-07 18:16:52 +0800

    Dynamically checks for start/end dates

--- main.rb
+++ main.rb
@@ -3,22 +3,23 @@ require "pry-byebug"
 # stock_picker([17,3,6,9,15,8,6,1,10])
  # => [1,4]  # for a profit of $15 - $3 == $12
 
-
 def stock_picker(list_prices)
-  lowest = { price: Float::INFINITY, day: nil }
-  highest = { price: -Float::INFINITY, day: nil } 
-  list_prices.each_with_index do |price, index|
-    if highest[:price] < price
-      highest[:price] = price
-      highest[:day] = index
-    end
-    if lowest[:price] > price 
-      lowest[:price] = price
-      lowest[:day] = index
+  tired = { buy_price: Float::INFINITY, buy_day: nil, sell_price: -Float::INFINITY, sell_day: nil, profit: -Float::INFINITY }
+  profit = 0
+  list_prices.each_with_index do |start_price, start_day|
+    list_prices.each_with_index do |end_price, end_day| # nested loop 
+      # puts ("start_price = #{start_price}, start_day = #{start_day}, end_price = #{end_price}, end_day = #{end_day}")
+      if ((end_price - start_price) > profit) && (end_day > start_day)
+        tired[:buy_price] = start_price
+        tired[:buy_day] = start_day
+        tired[:sell_day] = end_day
+        tired[:sell_price] = end_price
+        profit = end_price - start_price
+      end
     end
   end
-  results = [lowest[:day], highest[:day]]
-  binding.pry
+  results = [tired[:buy_day], tired[:sell_day]]
+  # binding.pry
   p results
 end
 
